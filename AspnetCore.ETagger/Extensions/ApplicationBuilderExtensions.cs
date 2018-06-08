63f283815d6a26d761b4a8730612adc98eea19f7
rsantosdev <rsantos@braziliandevs.com>
2018-06-08 11:15:44 -0300

    ETag middleware

---
+++ AspnetCore.ETagger/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using AspnetCore.ETagger.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace AspnetCore.ETagger.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UseETagger(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ETagMiddleware>();
+        }
+    }
+}
